ead4771bb8f107de943f6c69b54a08d4b2fe6f2d
causehhc <1005694755@qq.com>
2021-03-30 19:48:24 +0800

    Online testV0.2

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,9 +5,9 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // baseURL: 'http://localhost:5000',
-  baseURL: 'http://39.97.120.75',
+  // baseURL: 'http://39.97.120.75',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
